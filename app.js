4ce84838f1e777ea2a5ce7cb947a680c6239b938
didiguoguo <yandiguoguo@163.com>
2019-06-14 16:51:23 +0800

    /

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors({
         if (ctx.url === '/test') {
             return "*"; // 允许来自所有域名请求
         }
-        return 'http://localhost:8080'; / 这样就能只允许 http://localhost:8080 这个域名的请求了
+        return 'http://localhost:8080'; // 这样就能只允许 http://localhost:8080 这个域名的请求了
     },
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
     maxAge: 5,
