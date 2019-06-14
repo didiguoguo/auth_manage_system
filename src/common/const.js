102b7473625cc1b21b06e32d03c24538cc58c084
diguoguo <yandiguoguo@163.com>
2019-06-14 16:05:09 +0800

    host

--- src/common/const.js
+++ src/common/const.js
@@ -1,5 +1,5 @@
 // const HOST = 'http://192.168.1.198:8999/api/v1';
-const HOST = 'http://39.98.233.207:8080/api/v1';
+const HOST = 'http://39.98.233.207:8080';
 export const LOGIN_HOST = 'http://39.98.233.207:8080';
 // const HOST = '';
 
